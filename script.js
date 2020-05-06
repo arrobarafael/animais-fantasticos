3c118ffee176a69ed1a5d104d06e9144d5658b36
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-05-06 13:27:10 -0300

    Adicionar animação suave ao exibir as seções do site

--- script.js
+++ script.js
@@ -70,3 +70,23 @@ function initScrollSuave() {
   });
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const windowHalf = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowHalf < 0;
+      if (isSectionVisible) {
+        console.log("animar");
+        section.classList.add("ativo");
+      }
+    });
+  }
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
+initAnimacaoScroll();
