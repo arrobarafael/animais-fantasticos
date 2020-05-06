5b86b3ade663ccaaf536265ab8c39cf1bf2383f9
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-05-06 12:58:33 -0300

    Criado scroll automatico de transição ao selecionar link interno do site

--- script.js
+++ script.js
@@ -42,3 +42,31 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll("a[href^='#']");
+  console.log(linksInternos);
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // const topo = section.offsetTop;
+    // window.scrollTo(0, topo);
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: "smooth",
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
