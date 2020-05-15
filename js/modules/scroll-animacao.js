4821d14fbb36c38defe479decd1e48aff633c349
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-05-15 10:53:23 -0300

    Adicionada seção de números de animais junto com animação de contagem

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -8,6 +8,8 @@ export default function initAnimacaoScroll() {
       const isSectionVisible = sectionTop - windowHalf < 0;
       if (isSectionVisible) {
         section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
       }
     });
   }
