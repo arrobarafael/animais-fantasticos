0b7612ec8b58379647689f2e681e682fcff82705
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-05-24 17:50:14 -0300

    Refatorado ScrollSuave para classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,27 +1,34 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    "[data-menu = 'suave'] a[href^='#']"
-  );
+export default class initScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = {
+        behavior: "smooth",
+        block: "start",
+      };
+    } else {
+      this.options = options;
+    }
 
-  function scrollToSection(event) {
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
 
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-
-    // const topo = section.offsetTop;
-    // window.scrollTo(0, topo);
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: "smooth",
-    // });
+    section.scrollIntoView(this.options);
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
+    });
+  }
+
+  init() {
+    this.addLinkEvent();
+    return this;
+  }
 }
